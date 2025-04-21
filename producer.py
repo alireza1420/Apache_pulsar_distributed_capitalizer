aa6d48f0aef9dd5b5a5235e7ab43d705d2208fc5
alireza1420 <74127671+alireza1420@users.noreply.github.com>
2025-04-21 10:39:49 +0200

    Update producer.py
    Change in producer

--- producer.py
+++ producer.py
@@ -1,32 +1,3 @@
-
-import pulsar
-
-client = pulsar.Client("pulsar://localhost:6650")
-# Consumer to recive the splitted messages
-consumer = client.subscribe("persistent://demo-tenant/demo-ns/split-topic",subscription_name="word-processor")
-
-#producer to send messages to merge
-producer = client.create_producer('persistent://demo-tenant/demo-ns/merge-topic')
-
-print("Capitalizing the words ! ")
-
-try:
-        while True:
-                msg = consumer.receive()
-                words=msg.data().decode('utf-8')
-
-                processed_words=words.upper()
-                producer.send(processed_words.encode('utf-8'))
-                consumer.acknowledge(msg)
-except Exception as e:
-        print("Error:",e)
-        consumer.negative_acknowledge(msg)
-finally:
-        client.close()
-
-ubuntu@alireza-moghaddam-a2:~$ ^C
-ubuntu@alireza-moghaddam-a2:~$ sudo cat producer.py
-# producer.py
 import pulsar
 
 client = pulsar.Client('pulsar://localhost:6650')
